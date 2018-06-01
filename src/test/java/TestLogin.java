71d321e5d21b5d557a16a3b295a7c29d5c0655f9
PavelNejinskiy <808nps@gmail.com>
2018-06-01 20:09:36 +0300

    testJira

---
+++ src/test/java/TestLogin.java
@@ -0,0 +1,84 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class TestLogin {
+
+
+
+    private static WebDriver driver;
+
+
+    @Test
+    public static void testLogin() throws InterruptedException {
+
+        login();
+
+        Assert.assertEquals(driver.findElement(By.cssSelector(".aui-page-header-main > h1:nth-child(1)")).getText(), "System Dashboard");
+
+        close();
+    }
+
+    @Test
+    public static void testCreateIssue() throws InterruptedException {
+        login();
+
+        myElement(By.cssSelector("#browse_link")).click();
+        myElement(By.cssSelector("#admin_main_proj_link_lnk")).click();
+        myElement(By.cssSelector("#create_link")).click();
+        myElement(By.cssSelector("#summary")).sendKeys("Test Issue");
+        myElement(By.cssSelector("#create-issue-submit")).click();
+
+        close();
+
+    }
+
+    @Test
+    public static void testOpenIssue() throws InterruptedException {
+        login();
+        myElement(By.cssSelector("#find_link")).click();
+        myElement(By.cssSelector("#issue_lnk_15926_lnk")).click();
+        Assert.assertEquals(myElement(By.cssSelector("#summary-val")).getText(), "Test Issue");
+        close();
+    }
+
+
+
+    public static void open(String url)
+    {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\chromdriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(10, SECONDS);
+    }
+
+
+    public static void login() {
+        open("http://jira.hillel.it:8080");
+
+        myElement(By.cssSelector("#login-form-username")).sendKeys("Pavel");
+        myElement(By.cssSelector("#login-form-password")).sendKeys("droplles");
+        myElement(By.cssSelector("#login")).click(); 
+
+    }
+
+    public static void close() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.close();
+    }
+
+
+    public static WebElement myElement(By by)
+    {
+        WebElement element = driver.findElement(by);
+        return element;
+    }
+
+}
